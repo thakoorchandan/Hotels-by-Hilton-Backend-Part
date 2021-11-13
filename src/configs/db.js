1dea57e7c630a689b2c83b4bab47eaba8c0b01b0
Thakoor Chandan <thakoorchandan@gmail.com>
2021-11-13 16:15:02 +0530

    FunctionalCloud  DB

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const connect = () => {
   return mongoose.connect(
-    "mongodb+srv://thakoorchandan:chandan98@cluster0.hgd7w.mongodb.net/Hilton-Hilton?retryWrites=true&w=majority"
+    `mongodb+srv://thakoorchandan:${process.env.CLOUDKEY}@cluster0.hgd7w.mongodb.net/Hilton-Hilton?retryWrites=true&w=majority`
   );
 };
 
