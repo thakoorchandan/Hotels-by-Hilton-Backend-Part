5191fa0a3592bc09a60d7bb6a7f406df4fa2c0e7
Thakoor Chandan <thakoorchandan@gmail.com>
2021-11-13 16:10:06 +0530

    MongoDB Atlas Cloud connect

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 
 const connect = () => {
-  return mongoose.connect("mongodb://127.0.0.1:27017/HiltonHotels");
+  return mongoose.connect(
+    "mongodb+srv://thakoorchandan:chandan98@cluster0.hgd7w.mongodb.net/Hilton-Hilton?retryWrites=true&w=majority"
+  );
 };
 
 module.exports = connect;
